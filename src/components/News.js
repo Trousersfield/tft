8724413600208a466457c3715493ee16379788c2
benja <benjamin.hosenfeld@web.de>
2020-05-06 21:28:24 +0200

    adapt number of news cards on viewport. slidable news cards left and right

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { GoChevronRight, GoChevronLeft } from 'react-icons/go'
 
+let debounce = null
+
 class News extends React.Component {
   constructor (props) {
     super (props)
@@ -15,22 +17,46 @@ class News extends React.Component {
   componentDidMount () {
     this.setNewsCards()
     this.loadNews()
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.handleResize)
+
+    if (debounce) clearTimeout(debounce)
+  }
+
+  handleResize = () => {
+    console.log('handling resize')
+    if (debounce) clearTimeout(debounce)
+    console.log('actually doing sth')
+
+    debounce = setTimeout(async () => {
+      await this.setNewsCards()
+      this.slideNews(true, true)
+      this.debounce = null
+    }, 500)
   }
 
   async loadNews () {
     console.log('loading news')
-    this.setState({ news: [1, 2, 3, 4] },() => this.slideNews())
+    this.setState({ news: [1, 2, 3, 4] }, () => this.slideNews())
   }
 
-  setNewsCards () {
+  async setNewsCards () {
     const box = document.getElementById('news-container').getBoundingClientRect()
+    // take left and right navigation button + spacings into account
+    const newsContainerWidth = box.width - 100
     // w-128: 32em and default pixel size = 16px --> 16x32 = 512px
-    const numberOfNewsCards = Math.floor(box.width / (16 * 32))
-    this.setState({ numberOfNewsCards })
+    const numberOfNewsCards = Math.floor(newsContainerWidth / (16 * 32))
+    console.log('num of cards: ', numberOfNewsCards)
+    new Promise(resolve => {
+      this.setState({ numberOfNewsCards }, () => { resolve() })
+    })
   }
 
-  slideNews (right = true) {
-    console.log('sliding')
+  slideNews (right = true, keep = false) {
+    console.log('sliding news')
     const news = this.state.news
     const numberOfNewsCards = this.state.numberOfNewsCards < news.length ?
       this.state.numberOfNewsCards : news.length
@@ -40,11 +66,17 @@ class News extends React.Component {
     // init
     if (slidingWindow.start === -1) {
       Object.assign(slidingWindow, { start: 0, end: numberOfNewsCards })
+    } else if (keep) {
+      Object.assign(slidingWindow, {
+        start: slidingWindow.start,
+        end: slidingWindow.start + numberOfNewsCards
+      })
+      // need to check if keep element is last element!!!
     } else {
       // move sliding window
       if (right) {
-        slidingWindow.start = slidingWindow.end + 1
-        slidingWindow.end += (numberOfNewsCards + 1)
+        slidingWindow.start = slidingWindow.end
+        slidingWindow.end += numberOfNewsCards
         // check if out of bounds
         if (slidingWindow.end >= news.length) {
           const overflow = slidingWindow.end - news.length + 1
@@ -55,12 +87,12 @@ class News extends React.Component {
           }
         }
       } else {
-        slidingWindow.end = slidingWindow.start -1
-        slidingWindow.start -= (numberOfNewsCards + 1)
+        slidingWindow.end = slidingWindow.start
+        slidingWindow.start -= numberOfNewsCards
         // check if out of bounds
         if (slidingWindow.start < 0) {
           const overflow = slidingWindow.start * -1
-          slidingWindow.start = news.length - 1 - overflow
+          slidingWindow.start = news.length - overflow
           //
           if (slidingWindow.end < 0) {
             slidingWindow.end = slidingWindow.start + numberOfNewsCards
@@ -81,7 +113,10 @@ class News extends React.Component {
     const { displayedNews } = this.state
 
     return (
-      <div id="news-container" className="relative flex flex-no-wrap justify-center">
+      <div
+        id="news-container"
+        className="relative flex flex-no-wrap justify-center"
+      >
         <SlideNavButton
           direction="left"
           onSlide={() => this.slideNews(false)}
@@ -117,11 +152,13 @@ const SlideNavButton = (props) => {
 
   return (
     <button
+      className="w-10 h-10 my-auto bg-gray-200 rounded-lg text-center
+        hover:bg-gray-400"
       onClick={props.onSlide}
     >
       {direction === 'left' ?
-        <GoChevronLeft /> :
-        <GoChevronRight />
+        <GoChevronLeft className="m-auto" /> :
+        <GoChevronRight className="m-auto" />
       }
     </button>
   )
